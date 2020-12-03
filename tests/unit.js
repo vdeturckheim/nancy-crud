618001506c7f6917de753e5cacabb225323b40ee
vdeturckheim <vlad2t@hotmail.com>
2020-12-03 11:31:31 +0100

    add put endpoint

--- tests/unit.js
+++ tests/unit.js
@@ -1,10 +1,20 @@
 const Code = require('@hapi/code');
 const Lab = require('@hapi/lab');
+const Supertest = require('supertest');
+
+const app = require('../index').app;
 
 const { expect } = Code;
 const { it } = exports.lab = Lab.script();
 
-it('returns true when 1 + 1 equals 2', () => {
+it('should return the list of posts', () => {
 
-    expect(1 + 1).to.equal(2);
+    return Supertest(app)
+        .get('/posts')
+        .then((response) => {
+
+            expect(response.statusCode).to.equal(200);
+            expect(response.body).to.be.an.array();
+            expect(response.body).to.have.length(2);
+        });
 });
