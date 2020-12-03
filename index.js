82490beabfeec1e94f47e426bd101c83a36a77f4
vdeturckheim <vlad2t@hotmail.com>
2020-12-03 09:47:10 +0100

    add nody parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Express = require('express');
+const BP = require('body-parser');
 const app = Express();
 
 const DB = require('./db');
 
+app.use(BP.json());
 app.get('/posts', (req, res, next) => {
 
     DB.all('SELECT * FROM POSTS', (err, rows) => {
